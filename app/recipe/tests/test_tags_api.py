e520419284fc9565a70b70b0b80491a147ededc8
Samuel Zuniga Vanoye <samuelzv@gmail.com>
2024-11-24 19:09:57 -0600

    Fix tags API tests and add update/delete functionality
    Refactor the tags API tests to use classes instead of functions. Fix typos
    in method names. Add tests for updating and deleting tags. Update the
    TagViewSet to include DestroyModelMixin and UpdateModelMixin for handling
    tag updates and deletions.

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -15,27 +15,32 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse("recipe:tag-list")
 
 
+def detail_url(tag_id):
+    """Create and return a tag url"""
+    return reverse("recipe:tag-detail", args=[tag_id])
+
+
 def create_user(email="user@example.com", password="testpass123"):
     """Create and return a new user"""
     return get_user_model().objects.create_user(email, password)
 
 
-def PublicTagsApiTests(TestCase):
+class PublicTagsApiTests(TestCase):
     """Test unaunthenticated API requests"""
 
-    def setup(self):
+    def setUp(self):
         self.client = APIClient()
 
     def test_auth_required(self):
-        res = self.cliente.get(TAGS_URL)
+        res = self.client.get(TAGS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-def PrivateTagsApiTests(TestCase):
+class PrivateTagsApiTests(TestCase):
     """Test authenticated API requests"""
 
-    def setup(self):
+    def setUp(self):
         self.client = APIClient()
         self.user = create_user()
         self.client.force_authenticate(self.user)
@@ -64,3 +69,26 @@ def PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]["name"], tag.name)
         self.assertEqual(res.data[0]["id"], tag.id)
+
+    def test_update_tag(self):
+        """Test updating a tag"""
+        tag = Tag.objects.create(user=self.user, name="After Dinner")
+
+        payload = {"name": "Dessert"}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload["name"])
+
+    def test_delete_tag(self):
+        """Test deleting a tag"""
+        tag = Tag.objects.create(user=self.user, name="Breakfast")
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,7 +38,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
     """Manage tags in the database"""
 
     serializer_class = TagSerializer
@@ -46,6 +51,6 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def get_query_set(self):
+    def get_queryset(self):
         """Filter queryset to authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by("-name")
