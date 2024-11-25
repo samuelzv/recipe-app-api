dfb7fb25181fd4676c0616323aad6124acae11a5
Samuel Zuniga Vanoye <samuelzv@gmail.com>
2024-11-25 17:06:56 -0600

    Add Ingredient model and API
    - Register Ingredient model in admin
    - Create Ingredient model and migration
    - Add Ingredient model to Recipe model
    - Create tests for Ingredient model
    - Add IngredientSerializer
    - Create Ingredient API endpoints and tests

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,11 +6,16 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import (
+    Recipe,
+    Tag,
+    Ingredient,
+)
 from recipe.serializers import (
     RecipeSerializer,
     RecipeDetailSerializer,
     TagSerializer,
+    IngredientSerializer,
 )
 
 
@@ -53,4 +58,22 @@ class TagViewSet(
 
     def get_queryset(self):
         """Filter queryset to authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+
+class IngredientViewSet(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    """Manage ingredients in the database."""
+
+    serializer_class = IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by("-name")
